9f60eaf6b9bf331f2ee63062f38842b1024e3416
Tom Taylor <tom.taylor@uswitch.com>
2017-04-03 23:35:20 +0100

    Build tools

--- setup.py
+++ setup.py
@@ -5,6 +5,26 @@ setup(
     name='seismic',
     version='1.0',
     author='Tom Taylor',
+    author_email='tom+seismic@tomm.yt',
+    url='https://github.com/t0mmyt/seismic-detector',
     description='Seismic event detection and analysis',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=(
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'obspy',
+        'sqlalchemy',
+        'psycopg2',
+        'minio',
+        'msgpack-python',
+        'gunicorn',
+        'flask',
+        'flask-appconfig>=0.10',
+        'flask-api',
+        'requests',
+        'celery',
+        'redis',
+        'flask-api',
+    )
 )
